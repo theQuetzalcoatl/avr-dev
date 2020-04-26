42f8b81ccfb347a3afeff5b530286b142e9d22c2
theQuetzalcoatl <danielgerendai@gmail.com>
2020-04-26 18:42:10 +0200

    - Created folder structure of OS component, also created several source files

--- Components/HSI.h
+++ Components/HSI.h
@@ -8,7 +8,7 @@
 
 /*=======[ Temperature sensing ]=======*/
 #define TEMP_SENSOR_VALUE 	(1000U) /* ohms */
-#define TEMP_COEFF					() /* ohms/C° */ /*3,90802*10^3*/ /* TODO: convert to fixed point value */
+#define TEMP_COEFF					() 			/* ohms/C° */ /*3,90802*10^3*/ /* TODO: convert to fixed point value */
 #define SELF_HEATING_COEFF	(0,4f)	/* K/mW  -- I^2*R = 1mW at 0 C° */ /* TODO: convert to fixed point value */
 
 #define TEMP_SENSOR_CURRENT	(1U) 		/* mA */
