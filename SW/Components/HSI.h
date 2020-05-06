ee225a986bbc1374f5807aebbde6c9653eaa0de7
theQuetzalcoatl <danielgerendai@gmail.com>
2020-05-06 23:34:02 +0200

    HSI update - Supply voltage - IO treshold values

--- SW/Components/HSI.h
+++ SW/Components/HSI.h
@@ -1,15 +1,18 @@
 /***************************************************************************\
-** This file containt predefined values about everything related to the HW **
+|* This file containt predefined values about everything related to the HW *|
 \***************************************************************************/
 
 
 /*=======[ General ]=======*/
 #define CLK_FRQ (16000000UL) /* Hz */
+#define VCC			(5u) /* V */
+#define POS_EDGE_TRESHOLD (1.9f) /* TODO: convert to fixed point value */
+#define NEG_EDGE_TRESHOLD (1.4f) /* TODO: convert to fixed point value */
 
 /*=======[ Temperature sensing ]=======*/
 #define TEMP_SENSOR_VALUE 	(1000U) /* ohms */
 #define TEMP_COEFF					() 			/* ohms/C° */ /*3,90802*10^3*/ /* TODO: convert to fixed point value */
-#define SELF_HEATING_COEFF	(0,4f)	/* K/mW  -- I^2*R = 1mW at 0 C° */ /* TODO: convert to fixed point value */
+#define SELF_HEATING_COEFF	(0.4f)	/* K/mW  -- I^2*R = 1mW at 0 C° */ /* TODO: convert to fixed point value */
 
 #define TEMP_SENSOR_CURRENT	(1U) 		/* mA */
 
