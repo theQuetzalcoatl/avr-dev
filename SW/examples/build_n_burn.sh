8444c78813c09c6b8fbbca88c8a660fd05c18ca1
theQuetzalcoatl <danielgerendai@gmail.com>
2021-05-25 23:40:13 +0200

    * Renamed for a clearer purpose.

--- SW/examples/build_n_burn.sh
+++ SW/examples/build_n_burn.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 output_bin="example.bin";
+mcu="atmega128";
 
-avr-gcc -mmcu=atmega128 -Wall -O2 -o $output_bin $1 && \
-avr-size --format=avr --mcu=atmega128 example.bin && \
+compiler_flags="-mmcu=${mcu} -Wall -Wextra -O2";
+
+avr-gcc ${compiler_flags} -o $output_bin $1 && \
+avr-size --format=avr --mcu=${mcu} ${output_bin} && \
 avr-objcopy --only-section=.text --only-section=.data --output-target=ihex $output_bin example.hex && \
 avrdude -p m128 -b 19200 -P /dev/ttyUSB0 -c arduino -U flash:w:example.hex:i;
+
