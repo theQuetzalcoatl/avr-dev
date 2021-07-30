c4a6cbb9ceff5b136a4cb588a5dc3140cd39f238
theQuetzalcoatl <danielgerendai@gmail.com>
2021-07-30 05:02:28 +0200

    Corrected the amount of minimal stack required by a thread     * it was shorter by one, because I thought from 0 to 31 = 31. It does not.

--- SW/Application/Aztec/kernel/kernel_config.h
+++ SW/Application/Aztec/kernel/kernel_config.h
@@ -12,7 +12,7 @@
 
 #define AVAILABLE_SRAM (4096u) /* bytes */
 #define MAX_STACK_SIZE ((AVAILABLE_SRAM*7)/10) /* ~70% of all internal RAM */
-#define MIN_STACK_SIZE (2u + 31u + 1u + 1u  + 2u) /* required for context switching -> PC + R0->R31 + RAMPZ + SREG  + 2bytes stack overflow detection */
+#define MIN_STACK_SIZE (2u + 32u + 1u + 1u  + 2u) /* required for context switching -> PC + R0->R31 + RAMPZ + SREG  + 2bytes stack overflow detection */
 
 #define SYSTEM_TICK_IN_MS (2u)
 
