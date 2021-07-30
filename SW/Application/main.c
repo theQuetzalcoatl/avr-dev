aeabd09b4211af511201ec57642c345e5c1b3a2b
theQuetzalcoatl <danielgerendai@gmail.com>
2021-07-30 03:48:19 +0200

    Added a testing main file, created 2 working, sperarate threads.

---
+++ SW/Application/main.c
@@ -0,0 +1,44 @@
+#include "Aztec/kernel/kernel.h"
+
+void thread_1(void)
+{
+    while(1){
+        if(button_get_button_state(BUTTON_2) == PIN_LOW){
+            uart_send('U');
+            _delay_ms(120);
+        }
+    }
+}
+
+void thread_2(void)
+{
+    while(1){
+        if(button_get_button_state(BUTTON_3) == PIN_LOW) turn_led_on(LED4);
+        else turn_led_off(LED4);
+    }
+}
+
+void thread_3(void)
+{
+    while(1){
+        toggle_led(LED1);
+        _delay_ms(123);
+    }
+}
+
+int main(void)
+{
+    uint8_t thread_1_stack[MIN_STACK_SIZE+30];
+    kernel_register_thread(thread_1, thread_1_stack, MIN_STACK_SIZE+30);
+    /*
+    uint8_t thread_2_stack[MIN_STACK_SIZE+30];
+    kernel_register_thread(thread_2, thread_2_stack, MIN_STACK_SIZE+30);
+    */
+    
+    uint8_t thread_3_stack[MIN_STACK_SIZE+30];
+    kernel_register_thread(thread_3, thread_3_stack, MIN_STACK_SIZE+30);
+
+    kernel_init_os();
+ 
+    return 0;
+}
