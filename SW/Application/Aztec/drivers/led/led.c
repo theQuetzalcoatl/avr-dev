04800c579d1e4618313231c4e6e632f0e061e011
theQuetzalcoatl <danielgerendai@gmail.com>
2021-10-12 20:42:37 +0200

    * Changed driver system   * Short: Now new function pair will be implemented(lease, release) which are   analogous to 'open' and 'close'. The caller gets a device ID, and with it, can call   certain functions of devices. If the wrong device ID is given, the function will return with an error.   Not all device functions will require a device ID. Only the ones like LED handling, whereas button state query is free for all.   * Implemented new functions using ID system   * Now each led has its on functions.

--- SW/Application/Aztec/drivers/led/led.c
+++ SW/Application/Aztec/drivers/led/led.c
@@ -1,6 +1,12 @@
 #include "led.h"
 #include "../../kernel/kernel.h"
 
+/**** DEIVCE ID ****/
+static uint8_t leased_1 = FALSE;
+static uint8_t leased_2 = FALSE;
+static uint8_t leased_3 = FALSE;
+static uint8_t leased_4 = FALSE;
+
 /* Device initialization */
 
 static void set_gpio_to_output(void);
@@ -23,26 +29,183 @@ static void turn_all_leds_off(void)
 
 /* Device functionalities */
 
-void turn_led_on(uint8_t led)
+device_id_t led_1_lease(void)
 {
-    if(led >= LED1 || led <= LED4){
+    if(leased_1 == FALSE){
+        leased_1 = TRUE;
+        return LED1_ID;
+    }
+    else return INVALID_ID;
+}
+
+void led_1_release(void)
+{
+    leased_1 = FALSE;
+}
+
+device_id_t led_2_lease(void)
+{
+    if(leased_2 == FALSE){
+        leased_2 = TRUE;
+        return LED2_ID;
+    }
+    else return INVALID_ID;
+}
+
+void led_2_release(void)
+{
+    leased_2 = FALSE;
+}
+
+device_id_t led_3_lease(void)
+{
+    if(leased_3 == FALSE){
+        leased_3 = TRUE;
+        return LED3_ID;
+    }
+    else return INVALID_ID;
+}
+
+void led_3_release(void)
+{
+    leased_3 = FALSE;
+}
+
+device_id_t led_4_lease(void)
+{
+    if(leased_4 == FALSE){
+        leased_4 = TRUE;
+        return LED4_ID;
+    }
+    else return INVALID_ID;
+}
+
+void led_4_release(void)
+{
+    leased_4 = FALSE;
+}
+
+
+k_error_t led_1_on(device_id_t id)
+{
+    if(id == LED1_ID){
         KERNEL_ENTER_ATOMIC();
-        PORTC |= 1<<led;
+        PORTC |= 1<<LED1;
         KERNEL_EXIT_ATOMIC();
+        return NO_ERROR;
     }
+    else return K_ERR_INVALID_DEVICE_ACCESS;
 }
 
-void turn_led_off(uint8_t led)
+k_error_t led_2_on(device_id_t id)
 {
-    if(led >= LED1 || led <= LED4){
-            KERNEL_ENTER_ATOMIC();
-        PORTC &= ~(1<<led);  
+    if(id == LED2_ID){
+        KERNEL_ENTER_ATOMIC();
+        PORTC |= 1<<LED2;
         KERNEL_EXIT_ATOMIC();
+        return NO_ERROR;
     }
+    else return K_ERR_INVALID_DEVICE_ACCESS;
 }
 
-void toggle_led(uint8_t led)
+k_error_t led_3_on(device_id_t id)
 {
-    if(led >= LED1 || led <= LED4) PORTC ^= 1<<led;
+    if(id == LED3_ID){
+        KERNEL_ENTER_ATOMIC();
+        PORTC |= 1<<LED3;
+        KERNEL_EXIT_ATOMIC();
+        return NO_ERROR;
+    }
+    else return K_ERR_INVALID_DEVICE_ACCESS;
 }
 
+k_error_t led_4_on(device_id_t id)
+{
+    if(id == LED4_ID){
+        KERNEL_ENTER_ATOMIC();
+        PORTC |= 1<<LED4;
+        KERNEL_EXIT_ATOMIC();
+        return NO_ERROR;
+    }
+    else return K_ERR_INVALID_DEVICE_ACCESS;
+}
+
+k_error_t led_1_off(device_id_t id)
+{
+    if(id == LED1_ID){
+        KERNEL_ENTER_ATOMIC();
+        PORTC &= ~(1<<LED1);  
+        KERNEL_EXIT_ATOMIC();
+        return NO_ERROR;
+    }
+    else return K_ERR_INVALID_DEVICE_ACCESS;
+}
+
+k_error_t led_2_off(device_id_t id)
+{
+    if(id == LED2_ID){
+        KERNEL_ENTER_ATOMIC();
+        PORTC &= ~(1<<LED2);  
+        KERNEL_EXIT_ATOMIC();
+        return NO_ERROR;
+    }
+    else return K_ERR_INVALID_DEVICE_ACCESS;
+}
+
+k_error_t led_3_off(device_id_t id)
+{
+    if(id == LED3_ID){
+        KERNEL_ENTER_ATOMIC();
+        PORTC &= ~(1<<LED3);  
+        KERNEL_EXIT_ATOMIC();
+        return NO_ERROR;
+    }
+    else return K_ERR_INVALID_DEVICE_ACCESS;
+}
+
+k_error_t led_4_off(device_id_t id)
+{
+    if(id == LED4_ID){
+        KERNEL_ENTER_ATOMIC();
+        PORTC &= ~(1<<LED4);  
+        KERNEL_EXIT_ATOMIC();
+        return NO_ERROR;
+    }
+    else return K_ERR_INVALID_DEVICE_ACCESS;
+}
+
+k_error_t led_1_toggle(device_id_t id)
+{
+    if(id == LED1_ID){
+        PORTC ^= 1<<LED1;
+        return NO_ERROR;
+    }
+    else return K_ERR_INVALID_DEVICE_ACCESS;
+}
+
+k_error_t led_2_toggle(device_id_t id)
+{
+    if(id == LED2_ID){
+        PORTC ^= 1<<LED2;
+        return NO_ERROR;
+    }
+    else return K_ERR_INVALID_DEVICE_ACCESS;
+}
+
+k_error_t led_3_toggle(device_id_t id)
+{
+    if(id == LED3_ID){
+        PORTC ^= 1<<LED3;
+        return NO_ERROR;
+    }
+    else return K_ERR_INVALID_DEVICE_ACCESS;
+}
+
+k_error_t led_4_toggle(device_id_t id)
+{
+    if(id == LED4_ID){
+        PORTC ^= 1<<LED4;
+        return NO_ERROR;
+    }
+    else return K_ERR_INVALID_DEVICE_ACCESS;
+}
--- SW/Application/Aztec/drivers/led/led.h
+++ SW/Application/Aztec/drivers/led/led.h
@@ -8,9 +8,27 @@
 #define LED3 PINC4
 #define LED4 PINC5
 
+
 extern void led_init_device(void);
-extern void turn_led_on(uint8_t led);
-extern void turn_led_off(uint8_t led);
-extern void toggle_led(uint8_t led);
+extern device_id_t led_1_lease(void);
+extern device_id_t led_2_lease(void);
+extern device_id_t led_3_lease(void);
+extern device_id_t led_4_lease(void);
+extern void led_1_release(void);
+extern void led_2_release(void);
+extern void led_3_release(void);
+extern void led_4_release(void);
+extern k_error_t led_1_on(device_id_t id);
+extern k_error_t led_2_on(device_id_t id);
+extern k_error_t led_3_on(device_id_t id);
+extern k_error_t led_4_on(device_id_t id);
+extern k_error_t led_1_off(device_id_t id);
+extern k_error_t led_2_off(device_id_t id);
+extern k_error_t led_3_off(device_id_t id);
+extern k_error_t led_4_off(device_id_t id);
+extern k_error_t led_1_toggle(device_id_t id);
+extern k_error_t led_2_toggle(device_id_t id);
+extern k_error_t led_3_toggle(device_id_t id);
+extern k_error_t led_4_toggle(device_id_t id);
 
 #endif /* LED_H */
