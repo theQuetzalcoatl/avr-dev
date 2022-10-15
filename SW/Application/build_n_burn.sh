182f203a93c29390017e43e9155adc19420e8eac
theQuetzalcoatl <danielgerendai@gmail.com>
2022-10-15 23:29:49 +0200

    * object file removal is more elegent now.

--- SW/Application/build_n_burn.sh
+++ SW/Application/build_n_burn.sh
@@ -2,13 +2,6 @@
 
 function main()
 {
-	function remove_binaries()
-	{
-		for binary_file in $(find . -type f -name "*.o"); do
-			rm $binary_file;
-		done
-	}
-
 	local output_bin="application.bin";
 	local optimization="-O3";
 	local debug="-g1";
@@ -38,13 +31,12 @@ function main()
 	# ----- linking ----- #
 	avr-gcc -mmcu=${uC} -o ${output_bin} test_application.o ./Aztec/${archive_filename} || did_compile=$?;
 
-	remove_binaries;
+	find . -type f -name "*.o" -exec rm {} \;
 
 	avr-objdump --source ${output_bin} > app_disass.s;
 
 	avr-size --format=avr --mcu=${uC} ${output_bin} && \
 	avr-objcopy --only-section=.text --only-section=.data --only-section=.bss --only-section=.bootloader --only-section=.noinit --output-target=ihex $output_bin application.hex
-
 
 	# ----- invoking post processing scripts ----- #
 	./ToolsAndScripts/header_guard_checker.sh ./Aztec/
