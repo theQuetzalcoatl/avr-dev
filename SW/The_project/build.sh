309c4f6c710bfbcd2b1a504035ba2e5b3f7c398d
theQuetzalcoatl <danielgerendai@gmail.com>
2023-02-23 21:12:58 +0100

    * Updated build script, w/o hungarian + deleted some unnecessary stuff

--- SW/The_project/build.sh
+++ SW/The_project/build.sh
@@ -11,13 +11,13 @@ function main()
 
 	local did_compile=0;
 
-	# ----- making a lib from the OS ----- #
+	# ----- making a lib from the OS + drivers ----- #
 	local archive_filename="Aztec.a";
 
 	local c_files=$(find ./Aztec -type f -name "*.c");
 
 	for c_file in ${c_files}; do
-		avr-gcc -mmcu=${uC} ${warnings} -I /usr/lib/avr/include/ ${optimization} ${used_standard} ${debug} -c -o ${c_file/%.c/.o} ${c_file} || did_compile=$?;
+		avr-gcc -mmcu=${uC} ${warnings} -I /usr/lib/avr/include/ ${optimization} ${debug} -c -o ${c_file/%.c/.o} ${c_file} || did_compile=$?;
 	done
 
 	local object_files=$(find ./Aztec -type f -name "*.o");
@@ -26,21 +26,20 @@ function main()
 	avr-ar -r -s -c ./Aztec/${archive_filename} ${object_files};
 	find ./Aztec -type f -name "*.o" -exec rm {} \;
 
-	# ----- Application compilation ----- #
+	# ----- Compiling programs----- #
 	cd Applications;
 
 	for app_dir in $(ls -d */); do
 		bash "${app_dir}/build.sh";
-		if [[ $? != 0 ]] ; then echo "Nincs siker!"; did_compile=1; fi
+		if [[ $? != 0 ]] ; then did_compile=1; fi
 	done
 
 	cd ..;
 
-	avr-gcc -mmcu=${uC} ${warnings} -I /usr/lib/avr/include/ -I "${kernel_include_path}" ${optimization} ${used_standard} ${debug} -c -o main.o main.c || did_compile=$?;
-
-	object_files=$(find . -type f -name "*.o");
+	avr-gcc -mmcu=${uC} ${warnings} -I /usr/lib/avr/include/ -I "${kernel_include_path}" ${optimization} ${debug} -c -o main.o main.c || did_compile=$?;
 
 	# ----- linking ----- #
+	object_files=$(find . -type f -name "*.o");
 	avr-gcc -mmcu=${uC} -o ${output_bin} ${object_files} ./Aztec/${archive_filename} || did_compile=$?;
 
 	avr-objdump --source ${output_bin} > app_disass.s;
@@ -50,6 +49,8 @@ function main()
 
 	# ----- invoking post processing scripts ----- #
 	./ToolsAndScripts/header_guard_checker.sh ./Aztec/
+
+	find . -type f -name "*.o" -exec rm {} \;
 
 	if [[ "$1" == "upload" && ${did_compile} == 0 ]]; then
 		avrdude -p m128 -b 19200 -P /dev/ttyUSB0 -c arduino -U flash:w:application.hex:i;
